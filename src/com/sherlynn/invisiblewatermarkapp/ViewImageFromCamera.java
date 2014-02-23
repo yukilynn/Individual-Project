74d53715c38f9e428da8445dedd39e0f6cd3761c
yukilynn <sherlynn.yuki@gmail.com>
2014-02-23 16:10:24 +0800

    fixed out of memory error

--- src/com/sherlynn/invisiblewatermarkapp/ViewImageFromCamera.java
+++ src/com/sherlynn/invisiblewatermarkapp/ViewImageFromCamera.java
@@ -1,14 +1,20 @@
 package com.sherlynn.invisiblewatermarkapp;
 
 import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
 public class ViewImageFromCamera extends Activity implements View.OnClickListener { //display the image taken from the camera  
 	private ImageView view;
-	private String image;
+	private String gotImage;
+	private Bitmap bitmap;
+	private Drawable image;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,21 +23,24 @@ public class ViewImageFromCamera extends Activity implements View.OnClickListene
 		setContentView(R.layout.image_view);
 		
 		initialise();
-        
-//		image  = getIntent().getExtras().getParcelable("photo");        
-//		view.setImageBitmap(image); 
 		
 		Bundle getImage = this.getIntent().getExtras();
-		image = getImage.getString("photo");
+		gotImage = getImage.getString("photo");
 
-		view.setImageBitmap(BitmapFactory.decodeFile(image));
+		//view.setImageBitmap(BitmapFactory.decodeFile(image));
+		
+		BitmapFactory.Options options = new BitmapFactory.Options();
+
+		options.inSampleSize = 4;
+
+		bitmap = BitmapFactory.decodeFile(gotImage, options);
+		image = new BitmapDrawable(Resources.getSystem(), bitmap);
+		view.setImageDrawable(image);
 	}
 	
 	private void initialise() {
 		// TODO Auto-generated method stub
 		view = (ImageView) findViewById(R.id.ivDisplay);
-		if (view != null)
-		   view.setImageBitmap(null);
 	}
 
 	@Override
--- src/com/sherlynn/invisiblewatermarkapp/ViewImageFromGallery.java
+++ src/com/sherlynn/invisiblewatermarkapp/ViewImageFromGallery.java
@@ -1,7 +1,11 @@
 package com.sherlynn.invisiblewatermarkapp;
 
 import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -9,6 +13,8 @@ import android.widget.ImageView;
 public class ViewImageFromGallery extends Activity implements View.OnClickListener { //display the selected from the gallery  
 	private ImageView view;
 	private String gotImage;
+	private Bitmap bitmap;
+	private Drawable image;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +27,21 @@ public class ViewImageFromGallery extends Activity implements View.OnClickListen
 		Bundle getImage = this.getIntent().getExtras();
 		gotImage = getImage.getString("bitmap");
 
-		view.setImageBitmap(BitmapFactory.decodeFile(gotImage));
+		//view.setImageBitmap(BitmapFactory.decodeFile(gotImage));
+		
+		BitmapFactory.Options options = new BitmapFactory.Options();
+
+		options.inSampleSize = 4;
+
+		bitmap = BitmapFactory.decodeFile(gotImage, options);
+		image  = new BitmapDrawable(Resources.getSystem(), bitmap);
+		view.setImageDrawable(image);
  
 	}
 	
 	private void initialise() {
 		// TODO Auto-generated method stub
 		view = (ImageView) findViewById(R.id.ivDisplay);
-		if (view != null)
-			   view.setImageBitmap(null);
 	}
 
 	@Override
